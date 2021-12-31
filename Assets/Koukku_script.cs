f6021c9e58cffe9a6f1a1f13241a509bc789f3e7
Sampo Hakola <sampo.hakola@helsinki.fi>
2021-12-31 19:25:01 +0200

    kalastaminen toimii semisti, orava liikkuu ja hyppii

---
+++ Assets/Koukku_script.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Koukku_script : MonoBehaviour {
+    
+    [SerializeField]
+    float y_speed = 0.1f;
+    [SerializeField]
+    float accpt_dist = 30f;
+    bool returning = false;
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+        DistCheck();
+        WowAmIDead();
+    }
+
+    void Move() {
+        if(returning) {
+            ReturnKoukku();
+        } else {
+            MoveAwayKoukku();
+        }
+    }
+
+    void ReturnKoukku() {
+        transform.position += new Vector3(0,1*y_speed,0);
+    }
+
+    void MoveAwayKoukku() {
+        transform.position += new Vector3(0,-1*y_speed,0);
+    }
+
+    void DistCheck() {
+        Vector3 parentPos =  gameObject.transform.parent.position;
+        if(Vector3.Distance(parentPos, transform.position)>accpt_dist) {
+            returning = true;
+        }
+    }
+
+    void WowAmIDead() {
+        if(returning) {
+            Vector3 parentPos =  gameObject.transform.parent.position;
+            if(Vector3.Distance(parentPos, transform.position)<1) {
+                Destroy(gameObject);
+                print("KILL");
+            }
+        }
+    }
+
+}
